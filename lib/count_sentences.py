c92fe1a0930260d616e0a0ebaf97a683b2dc5717
Melissa Wang <melissa.wang21@gmail.com>
2024-10-07 18:15:24 -0400

    counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value="String"):
+    self.value = value
+
+  @property
+  def value(self):
+    """The value property"""
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    return (self._value[-1] == ".")
+  
+  def is_question(self):
+    return (self._value[-1] == "?")
+  
+  def is_exclamation(self):
+    return (self._value[-1] == "!")
+  
+  def count_sentences(self):
+    tmp = self._value.replace("???", "?", -1)
+    tmp = tmp.replace("??", "?", -1)
+    tmp = tmp.replace("?", ".", -1)
+    tmp = tmp.replace("!!!", "!", -1)
+    tmp = tmp.replace("!!", "!", -1)
+    tmp = tmp.replace("!", ".", -1)
+    tmp = tmp.replace("...", ".", -1)
+    tmp = tmp.replace("..", ".", -1)
+    
+    tmp = tmp.split(".")
+    return len(tmp) - 1
